72ec0b6562103fe08bd10eba793b85e8262d9258
unknown <vksingh112001@gmail.com>
2026-06-27 22:25:03 +0530

    Syntax used in python

---
+++ Syntex/syntax.py
@@ -0,0 +1,7 @@
+#Python allows only 1 minimum space at indent or commonly use 4
+#A computer program is a list of "instructions" to be "executed" by a computer.
+#In a programming language, these programming instructions are called statements.
+#The following statement prints the text "Python is fun!" to the screen:
+
+#Semicolons are optional in Python. You can write multiple statements on one line by separating them with ; but this is rarely used because it makes it hard to read:
+print("Hello"); print("How are you?"); print("Bye bye!")
